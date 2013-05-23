e4c9732a327a7ce2e6c5e973c6a86392812a47f3
Logan Jeya <vithu.jeya@gmail.com>
2013-05-23 00:41:10 -0700

    2D + 3D agents + cv script

---
+++ 3D Perception Agent/StackOfInts.java
@@ -0,0 +1,69 @@
+
+/* Stack of ints 
+ * 
+ */
+public class StackOfInts
+{
+    int size;
+    int[] data;
+    final int INIT_SIZE = 100;
+
+    /**
+     * Constructor for objects of class StackOfShorts
+     */
+    public StackOfInts()
+    {
+        size = 0;
+        data = new int[ INIT_SIZE ];
+    }
+
+    
+    private void resize()
+    {
+        int[] newData = new int[ data.length * 2 ];
+        for ( int i = 0; i < data.length; i++ )
+            newData[i] = data[i];
+        data = newData;
+    }
+    
+    public boolean isEmpty()
+    {
+        return ( size==0 );
+    }
+    
+    public int pop()
+    {
+        if ( size > 0 )
+        {
+            size--;
+            return data[size];
+        }
+        else
+        {
+            System.out.println("Popping from an empty stack.");
+            return( -1 );
+        }
+    }
+    
+    public int top()
+    {
+        if ( size > 0 )
+        {
+            return data[size-1];
+        }
+        else
+        {
+            System.out.println("Popping from an empty stack.");
+            return( -1 );
+        }
+    }
+    public void push( int newItem )
+    {
+        if ( size == data.length )
+            resize();
+        
+        data[size] = newItem;
+        size++;
+        return;
+    }
+}
